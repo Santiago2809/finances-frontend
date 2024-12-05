8737ebf0789d2e0797a934ace0aa2dd1c641d395
Santiago Cecena <santycc00@hotmail.com>
2024-12-05 12:22:26 -0700

    feat: add cookie credentials to axios method and handle errors if request fail

--- src/components/Auth/services/login.ts
+++ src/components/Auth/services/login.ts
@@ -1,35 +1,40 @@
 import axios, { AxiosError } from "axios";
 
 
-async function login(email: string, password: string): Promise<string> {
+async function login(email: string, password: string): Promise<void> {
 
-    const token: string = "";
 
     try {
 
         const response = await axios({
             method: 'post',
             url: "http://localhost:3000/auth/login",
+            withCredentials: true,
             data: {
-                username: email,
+                email,
                 password
             }
         })
+        console.log(response);
         if (response.status === 200) {
-            return "token";
+            return Promise.resolve();
         }
+
     } catch (error) {
 
         if (error instanceof AxiosError) {
+            if (error.status === 400) {
+                return Promise.reject("Please enter valid fields.")
+            }
             if (error.status === 401) {
-                return Promise.reject("Incorrect username or password")
+                return Promise.reject("Password is incorrect.");
+            }
+            if (error.status === 404) {
+                return Promise.reject("Email not found, please try again.");
             }
         }
         return Promise.reject("Something went wrong, try again later.");
     }
-
-    return token;
-
 }
 
 export default login;
