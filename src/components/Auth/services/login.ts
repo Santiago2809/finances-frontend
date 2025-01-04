e1267eb38d1ee924760b1042959c3f66e9fbe840
Santiago Cecena <santycc00@hotmail.com>
2025-01-04 00:18:25 -0700

    feat: refactor login service to use axios instance for API calls

--- src/components/Auth/services/login.ts
+++ src/components/Auth/services/login.ts
@@ -1,40 +1,42 @@
 import axios, { AxiosError } from "axios";
-
+import { axiosInstance } from "../../../config/axios-config";
 
 async function login(email: string, password: string): Promise<void> {
-
-
-    try {
-
-        const response = await axios({
-            method: 'post',
-            url: "http://localhost:3000/auth/login",
-            withCredentials: true,
-            data: {
-                email,
-                password
-            }
-        })
-        console.log(response);
-        if (response.status === 200) {
-            return Promise.resolve();
-        }
-
-    } catch (error) {
-
-        if (error instanceof AxiosError) {
-            if (error.status === 400) {
-                return Promise.reject("Please enter valid fields.")
-            }
-            if (error.status === 401) {
-                return Promise.reject("Password is incorrect.");
-            }
-            if (error.status === 404) {
-                return Promise.reject("Email not found, please try again.");
-            }
-        }
-        return Promise.reject("Something went wrong, try again later.");
-    }
+	try {
+		// const response = await axios({
+		//     method: 'post',
+		//     url: "http://localhost:3000/auth/login",
+		//     withCredentials: true,
+		//     data: {
+		//         email,
+		//         password
+		//     }
+		// })
+		const response = await axiosInstance.post(
+			"/auth/login",
+			{ email, password },
+			{
+				withCredentials: true,
+			}
+		);
+		console.log(response);
+		if (response.status === 200) {
+			return Promise.resolve();
+		}
+	} catch (error) {
+		if (error instanceof AxiosError) {
+			if (error.status === 400) {
+				return Promise.reject("Please enter valid fields.");
+			}
+			if (error.status === 401) {
+				return Promise.reject("Password is incorrect.");
+			}
+			if (error.status === 404) {
+				return Promise.reject("Email not found, please try again.");
+			}
+		}
+		return Promise.reject("Something went wrong, try again later.");
+	}
 }
 
 export default login;
