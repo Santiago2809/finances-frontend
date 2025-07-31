b38a4cf74559ba96ae5f14006b54d0a63c0abd03
Santiago Cecena <santycc00@hotmail.com>
2025-07-31 01:24:10 -0700

    feat: add TransactionsPage route and error handling to router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,43 +5,51 @@ import MainDashboard from "../components/Dashboard/MainDashboard";
 import { baseLoader } from "./loaders";
 import { lazy, Suspense } from "react";
 import FallbackLoader from "../components/Ui/FallbackLoader";
+import ErrorElement from "../components/Ui/ErrorElement";
+import TransactionsPage from "../components/Transactions/TransactionsPage";
 
-const Register = lazy(() => import("../components/Auth/Register"))
-
+const Register = lazy(() => import("../components/Auth/Register"));
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        loader: baseLoader,
-        children: [
-            {
-                path: "",
-                loader: baseLoader,
-                element: <MainDashboardLayout />,
-                children: [
-                    {
-                        index: true,
-                        element: <MainDashboard />
-                    }
-                ]
-            },
-            {
-                path: "auth",
-                children: [
-                    {
-                        path: "login",
-                        element: <Login />
-                    },
-                    {
-                        path: "register",
-                        element: <Suspense fallback={<FallbackLoader />}>
-                            <Register />
-                        </Suspense>
-                    }
-                ]
-            }
-        ],
-    },
+	{
+		path: "/",
+		loader: baseLoader,
+		children: [
+			{
+				path: "",
+				loader: baseLoader,
+				element: <MainDashboardLayout />,
+				children: [
+					{
+						index: true,
+						element: <MainDashboard />,
+					},
+					{
+						path: "transactions",
+						element: <TransactionsPage />,
+					},
+				],
+			},
+			{
+				path: "auth",
+				children: [
+					{
+						path: "login",
+						element: <Login />,
+					},
+					{
+						path: "register",
+						element: (
+							<Suspense fallback={<FallbackLoader />}>
+								<Register />
+							</Suspense>
+						),
+					},
+				],
+			},
+		],
+		errorElement: <ErrorElement />,
+	},
 ]);
 
 export default router;
