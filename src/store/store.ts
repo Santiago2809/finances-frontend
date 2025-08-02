4579fcc0a3d19511842c4e37ad743908c2a638dd
Santiago Cecena <santycc00@hotmail.com>
2025-08-02 02:46:40 -0700

    feat: create auth store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,33 @@
 import { create } from "zustand";
 
-
 type State = {
-    menuVisible: boolean
-}
+	menuVisible: boolean;
+};
 
 type Action = {
-    showMenu: () => void,
-    hideMenu: () => void
-}
+	showMenu: () => void;
+	hideMenu: () => void;
+};
 
 export const useUiStore = create<State & Action>((set) => ({
-    menuVisible: false,
-    showMenu: () => set(() => ({ menuVisible: true })),
-    hideMenu: () => set(() => ({ menuVisible: false }))
-}))
+	menuVisible: false,
+	showMenu: () => set(() => ({ menuVisible: true })),
+	hideMenu: () => set(() => ({ menuVisible: false })),
+}));
+
+type AuthState = {
+	isAuthenticated: boolean;
+	user: null | IUser;
+};
+
+type AuthActions = {
+	login: (user: IUser) => void;
+	logout: () => void;
+};
+
+export const useAuthStore = create<AuthState & AuthActions>((set) => ({
+	isAuthenticated: false,
+	user: null,
+	login: (user: IUser) => set(() => ({ isAuthenticated: true, user: user })),
+	logout: () => set(() => ({ isAuthenticated: false, user: null })),
+}));
