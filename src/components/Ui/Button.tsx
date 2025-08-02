f3e3bf5c899c0fc32c0dbfaf91ea4970254b7992
Santiago Cecena <santycc00@hotmail.com>
2025-08-02 02:47:14 -0700

    feat: enhance Button component to accept custom styles for improved flexibility

--- src/components/Ui/Button.tsx
+++ src/components/Ui/Button.tsx
@@ -1,13 +1,21 @@
+import { cn } from "../../lib/utils";
 
-export default function Button({ onClick, children, ...props }: { onClick?: React.MouseEventHandler<HTMLButtonElement> } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
-
-    return (
-        <button
-            className="px-6 py-3 bg-emerald-700 hover:bg-emerald-600 transition-colors duration-200 text-white rounded-lg disabled:bg-emerald-900 disabled:text-slate-300"
-            onClick={onClick || (() => { })}
-            {...props}
-        >
-            {children}
-        </button>
-    )
+export default function Button({
+	onClick,
+	children,
+	styles,
+	...props
+}: { onClick?: React.MouseEventHandler<HTMLButtonElement>; styles?: React.HTMLAttributes<HTMLButtonElement>["className"] } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+	return (
+		<button
+			className={cn([
+				"px-4 py-2 bg-emerald-700 hover:bg-emerald-600 transition-colors duration-200 text-white rounded-lg disabled:bg-emerald-900 disabled:text-slate-300",
+				styles as string,
+			])}
+			onClick={onClick || (() => {})}
+			{...props}
+		>
+			{children}
+		</button>
+	);
 }
