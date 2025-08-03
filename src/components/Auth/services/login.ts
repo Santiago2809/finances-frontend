5ac295cd0cd5df46a910b11f7167c8f84ccb7e37
Santiago Cecena <santycc00@hotmail.com>
2025-08-03 16:07:14 -0700

    refactor: update login function to return IUser type and clean up commented code

--- src/components/Auth/services/login.ts
+++ src/components/Auth/services/login.ts
@@ -1,27 +1,19 @@
 import axios, { AxiosError } from "axios";
 import { axiosInstance } from "../../../config/axios-config";
 
-async function login(email: string, password: string): Promise<void> {
+async function login(email: string, password: string): Promise<IUser | void> {
 	try {
-		// const response = await axios({
-		//     method: 'post',
-		//     url: "http://localhost:3000/auth/login",
-		//     withCredentials: true,
-		//     data: {
-		//         email,
-		//         password
-		//     }
-		// })
-		const response = await axiosInstance.post(
+		const response = await axiosInstance.post<IUser>(
 			"/auth/login",
 			{ email, password },
 			{
 				withCredentials: true,
 			}
 		);
-		console.log(response);
+		// console.log(response);
 		if (response.status === 200) {
-			return Promise.resolve();
+			const user = response.data;
+			return user;
 		}
 	} catch (error) {
 		if (error instanceof AxiosError) {
