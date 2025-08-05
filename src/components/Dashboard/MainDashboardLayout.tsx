72f5fd32e0b6aecd435d4f7cc43d536f5033f74b
Santiago Cecena <santycc00@hotmail.com>
2025-08-04 19:54:06 -0700

    refactor: simplify authentication handling with hook in MainDashboardLayout

--- src/components/Dashboard/MainDashboardLayout.tsx
+++ src/components/Dashboard/MainDashboardLayout.tsx
@@ -1,44 +1,22 @@
 import { Outlet } from "react-router-dom";
-import BottomNavigation from "../Ui/BottomNavigation";
 import Drawer from "../Ui/Drawer";
 import { PanelRight } from "lucide-react";
-import { useEffect } from "react";
-import { useAuthStore } from "../../store/store";
-import { axiosInstance } from "../../config/axios-config";
-// import { useUiStore } from "../../store/store";
-// import Drawer from "../Ui/Drawer";
+import { useValidateAuth } from "../../hooks/useValidateAuth";
 
 function MainDashboardLayout() {
-	const { user, login } = useAuthStore((state) => state);
-
-	useEffect(() => {
-		(async () => {
-			try {
-				const response = await axiosInstance.get<IUserResponse>("auth/me", { withCredentials: true });
-				if (response.status === 200) {
-					const userData = response.data.data;
-					if (userData) {
-						// Set user data in the store
-						login(userData);
-					}
-				}
-			} catch (error) {
-				console.log(error);
-			}
-		})();
-	}, [login]);
+	const user = useValidateAuth();
 
 	return (
 		<div className="flex h-screen">
 			<Drawer />
 			<main className="relative h-screen w-full">
 				{/* <BottomNavigation /> */}
-				<section className="flex-grow">
+				<section className="flex-grow h-full">
 					<div className="w-full flex items-center gap-x-3 bg-[#1f1f20] p-4">
 						<div className="p-2 hover:rounded-full hover:bg-slate-400/15 cursor-pointer">
 							<PanelRight size={20} />
 						</div>
-						{user && <p className="">Hello {user?.name.split(" ")[0]}</p>}
+						{user && <p className="">Hello {user?.name?.split(" ")[0]}</p>}
 					</div>
 					<div className="p-6">
 						<Outlet />
