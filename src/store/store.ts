995e57d318f7fea40d89bf5512c581c5bf9b410c
Santiago Cecena <santycc00@hotmail.com>
2025-08-08 22:04:25 -0700

    feat: enhance UI store with addTransactionModal visibility actions

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,17 +2,23 @@ import { create } from "zustand";
 
 type State = {
 	menuVisible: boolean;
+	addTransactionModalVisible: boolean;
 };
 
 type Action = {
 	showMenu: () => void;
 	hideMenu: () => void;
+	showAddTransactionModal: () => void;
+	hideAddTransactionModal: () => void;
 };
 
 export const useUiStore = create<State & Action>((set) => ({
 	menuVisible: false,
+	addTransactionModalVisible: false,
 	showMenu: () => set(() => ({ menuVisible: true })),
 	hideMenu: () => set(() => ({ menuVisible: false })),
+	showAddTransactionModal: () => set(() => ({ addTransactionModalVisible: true })),
+	hideAddTransactionModal: () => set(() => ({ addTransactionModalVisible: false })),
 }));
 
 type AuthState = {
@@ -30,4 +36,25 @@ export const useAuthStore = create<AuthState & AuthActions>((set) => ({
 	user: null,
 	login: (user: IUser) => set(() => ({ isAuthenticated: true, user: user })),
 	logout: () => set(() => ({ isAuthenticated: false, user: null })),
+}));
+
+type TransactionsState = {
+	transactions: ITransaction[];
+};
+
+type TransactionsActions = {
+	addTransactions: (transactions: ITransaction[]) => void;
+	addNewTransaction: (transaction: ITransaction) => void;
+};
+
+export const useTransactionsStore = create<TransactionsState & TransactionsActions>((set) => ({
+	transactions: [],
+	addTransactions: (transactions) =>
+		set(() => ({
+			transactions: transactions,
+		})),
+	addNewTransaction: (transaction) =>
+		set((state) => ({
+			transactions: [...state.transactions, transaction],
+		})),
 }));
