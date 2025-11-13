82c93f68613ae9b2d63bf19ef84ef9ecbf6940e3
Santiago Cecena <santycc00@hotmail.com>
2025-11-13 01:27:20 -0700

    style: remove Drawer icon if screen is not mobile-like

--- src/components/Dashboard/MainDashboardLayout.tsx
+++ src/components/Dashboard/MainDashboardLayout.tsx
@@ -17,10 +17,12 @@ function MainDashboardLayout() {
 			<main className="relative h-screen w-full">
 				<section className="flex-grow h-full overflow-y-scroll">
 					<div className="w-full flex items-center gap-x-3 bg-[#1f1f20] p-4">
-						<div className="p-2 hover:rounded-full hover:bg-slate-400/15 cursor-pointer" onClick={isMobile && drawerVisible ? hideDrawer : showDrawer}>
-							<PanelRight size={20} />
-						</div>
-						{user && <p className="">Hello {user?.name?.split(" ")[0]}</p>}
+						{isMobile && (
+							<div className="p-2 hover:rounded-full hover:bg-slate-400/15 cursor-pointer" onClick={isMobile && drawerVisible ? hideDrawer : showDrawer}>
+								<PanelRight size={20} />
+							</div>
+						)}
+						{user && <p className="min-[890px]:text-lg">Hello {user?.name?.split(" ")[0]}</p>}
 					</div>
 					<div className="p-6 pb-8">
 						<Outlet />
